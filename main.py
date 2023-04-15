332aad74ff193b06615f2763af27f03ac6f7b867
Daniel Itkes <danielitkes@gmail.com>
2023-04-15 14:45:12 +0300

    Added meals/{ID}, meals/{name} endpoints

--- main.py
+++ main.py
@@ -18,15 +18,20 @@ class DataCollection:
                        3: { 'id': 3, 'name': 'lasagna', 'cal': 1, 'sodium': 1, 'sugar': 1}}
     
     def get_dishes(self):
-        return jsonify(self.dishes)
+        return self.dishes
     
     def get_meals(self):
-        return jsonify(self.meals)
+        return self.meals
+    
     
     def add_meal(self, meal):
         id = meal['id']
         self.meals[id] = meal
         return id
+    
+    def delete_meal(self, id):
+        del self.meals[id]
+        
         
     # Generate ID for new datd items
     def get_id(self, type):
@@ -37,13 +42,13 @@ class DataCollection:
             self.dish_id_counter += 1
             return self.dish_id_counter
     
-    # Searches for a specific data item in data. returns -1 if item doesn't exists
+    # Searches for a specific data item in data. returns None if item doesn't exists
     def find_data_item(self, data, target_key, target_value):
         for item_key in data:
             if data[item_key][target_key] == target_value:
                 return data[item_key]
             
-        return -1
+        return None
     
     # Checks that the parameters of the POST requests are correct
     # 'required_params' should be "name_of_key": type_of_value" pairs. E.g. {'name': str, 'id': int}
@@ -66,7 +71,7 @@ class Dishes(Resource):
     global col
 
     def get(self):
-        return make_response(col.get_dishes(), 200)
+        return make_response(jsonify(col.get_dishes()), 200)
 
     def delete(self):
         return make_response({ }, 405)
@@ -76,7 +81,7 @@ class MealsList(Resource):
     global col
     
     def get(self):
-        return make_response(col.get_meals(), 200)
+        return make_response(jsonify(col.get_meals()), 200)
     
     def post(self):
         # Only accept app/json content type
@@ -91,7 +96,7 @@ class MealsList(Resource):
             return -1, 422
         
         # Check if a meal with that name already exists
-        if col.find_data_item(col.meals, 'name', req_json['name']) != -1:
+        if col.find_data_item(col.meals, 'name', req_json['name']) != None:
             return -2, 422
         
         # Calculate the total nutrition of the dishes, returns error if a dish doesn't exist
@@ -102,23 +107,6 @@ class MealsList(Resource):
         req_json['id'] = col.get_id('meal')
         id = col.add_meal(req_json)
         return id, 201
-    
-    ####################
-    # Helper Functions #
-    ####################
-    def validate_params(self, json):
-        required_params = {'name': str, 'appetizer': int, 'main': int, 'dessert': int}
-        
-        # Check if json has all the neccesary keys
-        if set(required_params.keys()) != set(json.keys()):
-            return False
-        
-        # Check if values have the correct type
-        for key in json:
-            if type(json[key]) != required_params[key]:
-                return False
-        
-        return True
     
     # Replace in the future with call to NinjaAPI
     def calculate_nutrition(self, json):
@@ -139,10 +127,87 @@ class MealsList(Resource):
 
         return True
         
+class MealByID(Resource):
+    global col
+    
+    def get(self, ID):
+        meal = col.meals.get(ID)
+            
+        if not meal:
+            return -5, 400
+
+        return make_response(jsonify(meal), 200)
+    
+    def delete(self, ID):
+        meal = col.meals.get(ID)
+            
+        if not meal:
+            return -5, 400
+        
+        col.delete_meal(self, ID)
+        return ID, 200
+    
+    def put(self, ID):
+        # Only accept app/json content type
+        if request.content_type != 'application/json':
+            return 0, 415
+        
+        req_json = request.get_json()
+        
+        # Check if params are specified correctly
+        required_params = {'name': str, 'appetizer': int, 'main': int, 'dessert': int, 'cal': float, 'sodium': float, 'sugar': float}
+        if not col.validate_params(req_json, required_params):
+            return -1, 422
+        
+        
+        
+        # Check whether we need to modify or create a new record
+        meal = col.meals.get(ID)
+        modify_existing_record = False if not meal else True
+        
+        # If we are creating a new record, we first check if a meal with that name already exists
+        if not modify_existing_record and col.find_data_item(col.meals, 'name', req_json['name']) != None:
+            return -2, 422
+        
+        # Add meal into database
+        if modify_existing_record:
+            req_json['id'] = ID
+        else:
+            req_json['id'] = col.get_id('meal')
+        
+        ID = col.add_meal(req_json)
+        
+        return ID, 201
+        
+    
+class MealByName(Resource):
+    global col
+    
+    def get(self, name):
+        meal = col.find_data_item(col.get_meals(), 'name', name)
+        
+        # Return an error if meal doesn't exit
+        if not meal:
+            return -5, 400
+        
+        return make_response(jsonify(meal), 200)
+    
+    def delete(self, name):
+        meal = col.find_data_item(col.get_meals(), 'name', name)
+        
+        # Return an error if meal doesn't exit
+        if not meal:
+            return -5, 400
+        
+        meal_id = meal['id']
+        col.delete_meal(meal_id)
+        return meal_id, 200
     
 
 api.add_resource(Dishes, '/dishes')
 api.add_resource(MealsList, '/meals')
+api.add_resource(MealByID, '/meals/<int:ID>')
+api.add_resource(MealByName, '/meals/<string:name>')
 
 if __name__ == '__main__':
     print('running main.py')

f4f1782b5c32d46e587a9d9cee4ded2335406e50
Daniel Itkes <danielitkes@gmail.com>
2023-04-18 16:12:45 +0300

    fixed duplicates and put method in meals/id

--- main.py
+++ main.py
@@ -9,51 +9,6 @@ from services import CalculateNutrition
 app = Flask(__name__)  # initialize Flask
 api = Api(app)  # create API
 
-class DataCollection:
-
-    def __init__(self):
-        self.meal_id_counter = 0
-        self.dish_id_counter = 0
-        
-        self.meals = {}
-        # TODO: CHANGE TO DYNAMIC
-        self.dishes = {1: { 'id': 1, 'name': 'lasagna', 'cal': 1, 'sodium': 1, 'sugar': 1},
-                       2: { 'id': 2, 'name': 'lasagna', 'cal': 1, 'sodium': 1, 'sugar': 1},
-                       3: { 'id': 3, 'name': 'lasagna', 'cal': 1, 'sodium': 1, 'sugar': 1}}
-    
-    def get_dishes(self):
-        return self.dishes
-    
-    def get_meals(self):
-        return self.meals
-    
-    
-    def add_meal(self, meal):
-        id = meal['id']
-        self.meals[id] = meal
-        return id
-    
-    def delete_meal(self, id):
-        del self.meals[id]
-        
-        
-    # Generate ID for new datd items
-    def get_id(self, type):
-        if type == 'meal':
-            self.meal_id_counter += 1
-            return self.meal_id_counter
-        elif type == 'dish':
-            self.dish_id_counter += 1
-            return self.dish_id_counter
-    
-    # Searches for a specific data item in data. returns None if item doesn't exists
-    def find_data_item(self, data, target_key, target_value):
-        for item_key in data:
-            if data[item_key][target_key] == target_value:
-                return data[item_key]
-            
-        return None
-
 col = DataCollection() 
 
 class Dishes(Resource):
@@ -127,7 +82,7 @@ class MealByID(Resource):
         req_json = request.get_json()
         
         # Check if params are specified correctly
-        validator = MealValidator(req_json, 'put').call()
+        validator = MealValidator(req_json, 'post').call()
         if not validator:
             return ResponseSerializer(-1, 422).serialize()
         
@@ -149,81 +104,6 @@ class MealByID(Resource):
         
         return ResponseSerializer(ID, 201).serialize()
         
-class MealByID(Resource):
-    global col
-    
-    def get(self, ID):
-        meal = col.meals.get(ID)
-            
-        if not meal:
-            return ResponseSerializer(-5, 400).serialize()
-
-        return ResponseSerializer(meal, 200).serialize()
-    
-    def delete(self, ID):
-        meal = col.meals.get(ID)
-            
-        if not meal:
-            return ResponseSerializer(-5, 400).serialize()
-        
-        col.delete_meal(self, ID)
-        return ResponseSerializer(ID, 200).serialize()
-    
-    def put(self, ID):
-        # Only accept app/json content type
-        if request.content_type != 'application/json':
-            return ResponseSerializer(0, 415).serialize()
-        
-        req_json = request.get_json()
-        
-        # Check if params are specified correctly
-        validator = MealValidator(req_json, 'put').call()
-        if not validator:
-            return ResponseSerializer(-1, 422).serialize()
-        
-        
-        
-        # Check whether we need to modify or create a new record
-        meal = col.meals.get(ID)
-        modify_existing_record = False if not meal else True
-        
-        # If we are creating a new record, we first check if a meal with that name already exists
-        if not modify_existing_record and col.find_data_item(col.meals, 'name', req_json['name']) != None:
-            return ResponseSerializer(-2, 422).serialize()
-        
-        # Add meal into database
-        if modify_existing_record:
-            req_json['id'] = ID
-        else:
-            req_json['id'] = col.get_id('meal')
-        
-        ID = col.add_meal(req_json)
-        
-        return ResponseSerializer(ID, 201).serialize()
-        
-    
-class MealByName(Resource):
-    global col
-    
-    def get(self, name):
-        meal = col.find_data_item(col.get_meals(), 'name', name)
-        
-        # Return an error if meal doesn't exit
-        if not meal:
-            return ResponseSerializer(-5, 400).serialize()
-        
-        return ResponseSerializer(meal, 200).serialize()
-    
-    def delete(self, name):
-        meal = col.find_data_item(col.get_meals(), 'name', name)
-        
-        # Return an error if meal doesn't exit
-        if not meal:
-            return ResponseSerializer(-5, 400).serialize()
-        
-        meal_id = meal['id']
-        col.delete_meal(meal_id)
-        return ResponseSerializer(meal_id, 200).serialize()
     
 class MealByName(Resource):
     global col
